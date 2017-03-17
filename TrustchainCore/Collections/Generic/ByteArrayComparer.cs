e029a08b239b98896cf5426b591743b04ad3a85d
keutmann <keutmann@yahoo.com>
2017-03-17 10:54:43 +0100

    Added Comparer

---
+++ TrustchainCore/Collections/Generic/ByteArrayComparer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TrustchainCore.Collections.Generic
+{
+    public class ByteArrayComparer : IEqualityComparer<byte[]>
+    {
+        public bool Equals(byte[] left, byte[] right)
+        {
+            if (left == null || right == null)
+                return left == right;
+
+            if (ReferenceEquals(left, right))
+                return true;
+
+            if (left.Length != right.Length)
+                return false;
+
+            return left.SequenceEqual(right);
+        }
+
+        public int GetHashCode(byte[] key)
+        {
+            if (key == null)
+                throw new ArgumentNullException("key");
+            return key.Sum(b => b);
+        }
+    }
+
+}
