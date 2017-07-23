1a5e3bdc00d275f7a8e709333afe288b51c2c98e
keutmann <keutmann@yahoo.com>
2017-07-23 23:41:36 +0200

    Optimized the Equals function from slow to fast

--- TrustchainCore/Collections/Generic/ByteArrayComparer.cs
+++ TrustchainCore/Collections/Generic/ByteArrayComparer.cs
@@ -1,31 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.ConstrainedExecution;
 
 namespace TrustchainCore.Collections.Generic
 {
-    public class ByteArrayComparer : IEqualityComparer<byte[]>
+
+    public static class ByteComparer
     {
-        public bool Equals(byte[] left, byte[] right)
+        private class StandardComparer : IEqualityComparer<byte[]>
         {
-            if (left == null || right == null)
-                return left == right;
+            public bool Equals(byte[] left, byte[] right)
+            {
+                if (left == null || right == null)
+                    return left == right;
 
-            if (ReferenceEquals(left, right))
+                if (ReferenceEquals(left, right))
+                    return true;
+
+                if (left.Length != right.Length)
+                    return false;
+
+                int index = 0;
+                while (index < left.Length)
+                {
+                    if (left[index] != right[index])
+                        return false;
+                    index++;
+                }
                 return true;
+            }
 
-            if (left.Length != right.Length)
-                return false;
-
-            return left.SequenceEqual(right);
+            public int GetHashCode(byte[] key)
+            {
+                if (key == null)
+                    throw new ArgumentNullException("key");
+                return key.Sum(b => b);
+            }
         }
 
-        public int GetHashCode(byte[] key)
-        {
-            if (key == null)
-                throw new ArgumentNullException("key");
-            return key.Sum(b => b);
-        }
+
+
+        public static readonly IEqualityComparer<byte[]> Standard = new StandardComparer();
     }
-
 }
